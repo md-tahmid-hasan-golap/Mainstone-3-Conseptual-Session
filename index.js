0d55bba4505d85e34d046aabb4665f944d42bd77
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-10-05 01:33:36 +0600

    resolve

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hellow World")
+// console.log("Hellow World")
+// var name = "Golap"
+// console.log(name)
+// amara javaScripts a 3 vabe variable diclare korte pari
+
+// 1 . var ---   ata user korbon na onek agar
+// 2. let -- ata used korbo  jodi man change korte chai 
+// 3. const -- ata update quared ata man poriborton korte na chaile used korbo
+
+
+
+// var name = "Golap";
+// console.log(name);
+// console.log(typeof name)
+// var age;
+// console.log(age)
